6f0ddb4bf3818dcf29d626bd6a8cf70024b57b68
amina-codes-lab <gulljaan5737@gmail.com>
2026-06-23 10:38:30 +0500

    first commit

---
+++ class1.py
@@ -0,0 +1,24 @@
+print("Hello World from Python Bro!")
+
+#DataTypes
+
+#string
+name = "Pyhton"
+print(name)
+
+#integer
+num = 100
+print(num)
+
+#float
+decimal = 30.96
+print(decimal)
+
+#boolean
+isTrue = True
+print(isTrue);
+
+#take input from user
+input = input("Enter your name: ")
+print(input)
+
